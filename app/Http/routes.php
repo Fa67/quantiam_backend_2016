fb40a5bfed611b6cc2c45821091ebf0d256a717c
chrispetrone <ctpetrone@gmail.com>
2016-05-13 10:43:47 -0600

    wtff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,21 +26,22 @@ Route::get('/', function () {
 |
 */
 
+Route::group(['middleware' => 'auth'], function () {
+
+Route::get('/rto/list/', 'RtoController@test'); // get all RTO requests applicable to this individual
+Route::get('/hr/{employee_id}/hiearchy', 'RtoController@hrHiearchyBuild'); // get all RTO requests applicable to this individual
+	
+	
+});
+
 Route::group(['middleware' => 'authuser'], function() 
 {
-<<<<<<< HEAD
-=======
 	Route::get('/authuser', 'RTOController@getAuthenticatedUser');
 
->>>>>>> develop
 	Route::get('/rto', 'RTOController@loadRTO');	// Load all self- and subordinate-RTOs
 	Route::get('/rto/{request_id}', 'RTOController@specRTO');	// Load specific rto
 	Route::post('/rto', 'RTOController@createRTO');  // Post a new row in the timesheet_rto table.
 	Route::put('/rto', 'RTOController@updateRTO');  // Edit existing RTO status.
-<<<<<<< HEAD
-	Route::get('/rto/request/callRTO', 'RTOController@callRTO');
-	Route::get('/rto/request/{request_id}', 'RTOController@requestSpecific');
-=======
 	Route::delete('/rto/{request_id', 'RTOController@deleteRTO'); // Delete and RTO and all its associated approvals/times.
 
 	Route::post('/rto/{request_id}/requestTime', 'RTOController@requestTime');
@@ -51,25 +52,17 @@ Route::group(['middleware' => 'authuser'], function()
 	Route::post('/rto/{request_id}/approval', 'RTOController@postApproval');
 
 	Route::post('/mail/send', 'MailController@send');
->>>>>>> develop
 
-	Route::post('/rto/{request_id}/requestTime', 'RTOController@requestTime');
-	Route::put('/rto/requestTime', 'RTOController@editRTOtime');
 
-	Route::put('/rto/approval/{approval_id}', 'RTOController@editApproval');
-	Route::post('/rto/{request_id}/approval', 'RTOController@postApproval');
 
-	Route::post('/mail/send', 'MailController@send');
 });
-// Create User JWT
-Route::post('/auth', 'RTOController@createUserToken'); // Create a JWT for a specific user.
-
 // Request existing RTOs
 // Post new approval.
 Route::post('/rto/request/{request_id}/approval', 'RTOController@postApproval');  //  Post a new row in the timesheet_rtoapprovals table relevant to an existing RTO id.
 // Edit existing approval
 Route::put('/rto/request/{request_id}/approval/{approval_id}', 'RTOController@updateApproval');  //  Submitted from Supervisor to approve/deny (?modify) an RTO.
 // Create a Json Web Token
+Route::post('/createJWT', 'RTOController@createUserToken'); // Create a JWT for a specific user.
 // Post a new RTO date/time
 Route::get('/user/{user_id}/getSubordinates', 'RTOController@getSubordinates');
 Route::get('/user/{user_id}/getSupervisors', 'RTOController@getSupervisors');
