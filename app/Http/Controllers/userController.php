ab930bf41521ea8ba71d8d1400ac0275ff24caee
chrispetrone <ctpetrone@gmail.com>
2016-05-16 16:12:39 -0600

    aeiouy

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Models\User;
+use App\Models\Nest;
 
 use Baum\Node;
 use Baum\Extensions\Query\Builder; 
@@ -12,21 +14,25 @@ use Baum\Extensions\Query\Builder;
 class userController extends Controller
 {
 
-	public function userInfo(Request $request)
+	public function userInfo($employee_id)
 	{
-		dd('slerp');
+		$employeeID = $employee_id;
+
+		$response = new User($employeeID, true);
+		dd($response);
 	}
 
 
     public function newUser(Request $request)
 	{
-		dd('merp');
+		dd($request -> all());
 	}
 
 
-	public function editUser(Request $request)
+	public function editUser(Request $request, $employee_id)
 	{
-		dd('klorp');
+		dd(Nest::where('employeeID', '=', $employee_id)->first());
+
 	}
 
 
