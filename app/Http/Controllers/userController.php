61d8c64cb487bd3c001d00074ca922af97dae24f
chrispetrone <ctpetrone@gmail.com>
2016-05-17 10:52:56 -0600

    Preliminary user searching function

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -8,11 +8,69 @@ use App\Http\Requests;
 use App\Models\User;
 use App\Models\Nest;
 
+use DB;
+
 use Baum\Node;
 use Baum\Extensions\Query\Builder; 
 
 class userController extends Controller
 {
+
+
+	public function searchUsers(Request $request, $isActive = true)
+	{	
+		// employeeID | email | ldap | firstname | lastname | title | compensation 
+		if (null != ($request -> input('isActive')) &&( ($request -> input('isActive')) == 'true' || ($request -> input('isActive')) == 'false') )
+		{	
+			$isActive = filter_var(($request -> input('isActive')), FILTER_VALIDATE_BOOLEAN);
+		}
+		else if (null != ($request -> input('isActive')) &&( ($request -> input('isActive')) != 'true' || ($request -> input('isActive')) != 'false') )
+		{
+			return response () -> json(['error' => 'Improper input of employee active status'], 406);
+		}
+
+		// setting search terms
+		$search = $request -> input('search');
+			$name = explode(' ', $search);
+			$firstname = $name[0];
+				$lastname = $name[0];
+					if (isset($name[1]))
+					{
+						$lastname = $name[1];
+					}
+		try
+		{
+			$response = DB::table('employees')	->where('employeeID', 'like', $search.'%')
+												->orWhere('email', 'like', $search.'%')
+												->orWhere('ldap_username', 'like', '%'.$search.'%')
+												->orWhere('firstname', 'like', $firstname)
+												->orWhere('lastname', 'like', $firstname)
+												->orWhere('lastname', 'like', $lastname)
+												->orWhere('compensation', 'like', $search.'%')
+												->orderBy('employeeid')
+												->get();
+
+		
+
+			if (isset($response[0]))
+			{
+				return response() -> json(['results' => $response], 200);
+			}
+			else {
+				return response() -> json(['results' => "Your search -- ".$search." -- did not match any documents."], 200);
+			}
+		}
+		catch (\Exception $e)
+		{
+			return response() -> json(['error' => $e]);
+		}
+
+
+
+
+
+	}
+
 
 	public function userInfo($employee_id)
 	{
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -57,7 +57,9 @@ Route::put('/rto/request/{request_id}/approval/{approval_id}', 'RTOController@up
 Route::get('/user/{user_id}/getSubordinates', 'RTOController@getSubordinates');
 Route::get('/user/{user_id}/getSupervisors', 'RTOController@getSupervisors');
 
+
 Route::get('/user/{employee_id}', 'userController@userInfo');
-Route::post('/user', 'userController@newUser');
+Route::post('/user/new', 'userController@newUser');
+Route::post('/user/', 'userController@searchUsers');
 Route::put('/user/{employee_id}', 'userController@editUser');
 Route::put('/hierarchy/{employee_id}', 'userController@moveUser');
