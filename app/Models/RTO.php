ca8f15c9520addd168f8ea48fc1635cca8d27118
chrispetrone <ctpetrone@gmail.com>
2016-06-07 16:45:31 -0600

    m

--- app/Models/RTO.php
+++ app/Models/RTO.php
@@ -91,41 +91,6 @@ class RTO extends Model
 
     }
 
-    public function checkRtoPermission($requestID, $rtotime = true)
-    {
-        if ($rtotime)
-        {
-            $requestID = DB::table('timesheet_rtotime') -> where ('rtotimeID', $requestID) -> value('requestID');
-        }
-
-        $approvals = DB::table('timesheet_rtoapprovals') ->where('requestID', $requestID) ->value('approval');
-
-                if ($approvals == null)
-                {
-                    return true;
-                }
-                else if ($approvals[0]){
-
-                    if($approvals[0]  -> employeeID == $request -> user -> employeeID) {
-                             return true;
-                       }
-                       else 
-                       {
-                        return false;
-                       }
-                }
-                else if ($approvals[1])
-                {
-
-                    if($approvals[1]  -> employeeID == $request -> user -> employeeID) {
-                             return true;
-                       }
-                       else 
-                       {
-                        return false;
-                       }
-                }
-    }
 
 
     public function postApproval($params, $depth)
