4541cf812b9f47ab80c5cf3e019b2090dd3570c7
chrispetrone <ctpetrone@gmail.com>
2016-06-08 08:45:26 -0600

    aa

--- app/Http/Controllers/RTOController.php
+++ app/Http/Controllers/RTOController.php
@@ -108,10 +108,10 @@ class RTOController extends Controller
 	{
 		$userInput = $request -> all();
 		$userInput['requestID'] = $request_id;
-
+/*
 		$permission = $this -> rto -> checkRtoPermission($request_id, false);
 		if ($permission)
-		{
+		{*/
 			try
 			{
 				$response = $this -> rto -> requestTime($userInput);
@@ -119,18 +119,18 @@ class RTOController extends Controller
 			}catch(\Exception $e){
 				return response() -> json(['error' => $e]);
 			}
-		}
+/*		}
 		else {
 			return response() -> json(['error' => "cannot post time request after approval has been posted"], 403);
-		}
+		}*/
 	}
 
 	public function editRTOtime(Request $request)
 	{	
 		$userInput = $request -> all();
-		$permission = $this -> rto -> checkRtoPermission($userInput ['rtotimeID']);
+/*		$permission = $this -> rto -> checkRtoPermission($userInput ['rtotimeID']);
 		if ($permission)
-		{
+		{*/
 			try
 			{
 				$response = $this -> rto -> editRTOtime($userInput);
@@ -140,11 +140,11 @@ class RTOController extends Controller
 			{
 				return response() -> json(['error' => $e], 401);
 			}
-		}
+/*		}
 		else 
 		{
 			return response() -> json (['error' => "Cannot edit after an approval has been posted"], 403);
-		}
+		}*/
 	}
 
 
@@ -153,8 +153,8 @@ class RTOController extends Controller
 	{
 
 		$permission = $this -> rto -> checkRtoPermission($rtotime_id);
-		if ($permission)
-		{
+/*		if ($permission)
+		{*/
 			try
 			{
 				$response = $this -> rto -> deleteRTOTime($rtotime_id);
@@ -164,11 +164,11 @@ class RTOController extends Controller
 			{
 				return response() -> json(['error' => $e]);
 			}
-		}
+/*		}
 		else 
 		{
 			return response() -> json(['error' => "Cannot delete after an approval has been posted"], 418);
-		}
+		}*/
 	}
 
 	public function postApproval(Request $request, $requestID)
