d0b90436cc87c34effe9b1855a5ca3e749a99f4a
chrispetrone <ctpetrone@gmail.com>
2016-06-16 08:26:45 -0600

    slip

--- app/Http/Controllers/SlipcastingController.php
+++ app/Http/Controllers/SlipcastingController.php
@@ -6,57 +6,14 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+Use App\Models\Slipcasting;
+
 class SlipcastingController extends Controller
 {
-    function getcsv(Request $request)
+    public function getcsv(Request $request)
     {
-        $slipcastID = $request -> input('slipcastID');
+        $response = new Slipcasting(['slipcastID' => $request -> input('slipcastID')]);
 
-        //-- Load csv data from file & explode into array of lines --
-    	$csvData = file_get_contents('C:\inetpub\wwwroot\api\storage\slipcasting\toluenedata\\'.$slipcastID.'.csv');
-        $lines = explode(PHP_EOL, $csvData);
-        $arrays = array();
-
-            foreach ($lines as $line)
-            {
-                $arrays[] = str_getcsv($line);
-            }
-        //-- Create object to store response on.
-        $response = app() -> make('stdClass');
-        $response -> title = $slipcastID;
-        $response -> dataset = array();
-
-        $columnCnt = count($arrays[14]);
-        $seriesCnt = $columnCnt - 3;
-        $seriesObjects = array();
-
-        for ($i = 0; $i < $seriesCnt; $i++)
-        {
-            $tempObject = app()->make('stdClass');
-            $tempObject -> title = $arrays[14][$i+3];
-            $tempObject -> x_label = 'Datetime';
-            $tempObject -> y_label = 'ppm Toluene';
-            $tempObject -> data = array();
-            $response -> dataset[] = $tempObject;
-
-
-        }
-
-        for ($i = 15; $i < count($arrays); $i++)
-        {
-            if (count($arrays[$i]) > 3) {
-                for ($k = 0; $k < $seriesCnt; $k++) {
-                    $tempObj = app()->make('stdClass');
-
-                    $tempObj->x = $arrays[$i][1] . " " . $arrays[$i][2];
-                    $tempObj->y = $arrays[$i][3 + $k];
-
-                    $response->dataset[$k]->data[] = $tempObj;
-                }
-            }
-
-        }
-
-        return response() -> json ($response);
+        return response() -> json (["csv data:" => $response], 200);
     }
 }
---
+++ app/Models/Slipcasting.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Http\Requests;
+
+class Slipcasting extends Model
+{
+
+    function __construct(array $attributes)
+    {   $this -> slipcastID = $attributes['slipcastID'];
+        $this -> csvData = $this -> getcsvData;
+    }
+
+
+    function getcsvData()
+    {
+        //-- Load csv data from file & explode into array of lines --
+        $csvData = file_get_contents('C:\inetpub\wwwroot\api\storage\slipcasting\toluenedata\\'.$this -> slipcastID.'.csv');
+        $lines = explode(PHP_EOL, $csvData);
+        $arrays = array();
+
+        foreach ($lines as $line)
+        {
+            $arrays[] = str_getcsv($line);
+        }
+        //-- Create object to store response on.
+        $response = app() -> make('stdClass');
+        $response -> title = $slipcastID;
+        $response -> dataset = array();
+
+        $columnCnt = count($arrays[14]);
+        $seriesCnt = $columnCnt - 3;
+        $seriesObjects = array();
+
+        for ($i = 0; $i < $seriesCnt; $i++)
+        {
+            $tempObject = app()->make('stdClass');
+            $tempObject -> title = $arrays[14][$i+3];
+            $tempObject -> x_label = 'Datetime';
+            $tempObject -> y_label = 'ppm Toluene';
+            $tempObject -> data = array();
+            $response -> dataset[] = $tempObject;
+
+
+        }
+
+        for ($i = 15; $i < count($arrays); $i++)
+        {
+            if (count($arrays[$i]) > 3) {
+                for ($k = 0; $k < $seriesCnt; $k++) {
+                    $tempObj = app()->make('stdClass');
+
+                    $tempObj->x = $arrays[$i][1] . " " . $arrays[$i][2];
+                    $tempObj->y = $arrays[$i][3 + $k];
+
+                    $response->dataset[$k]->data[] = $tempObj;
+                }
+            }
+
+        }
+
+        return response() -> json ($response);
+    }
+}
