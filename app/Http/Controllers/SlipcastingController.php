26d14126fa84426f0546c22f92914fbef44ce3e0
Tyson <Tyson>
2016-06-27 16:40:16 -0600

    Holy **** this is annoying to do.

--- app/Http/Controllers/SlipcastingController.php
+++ app/Http/Controllers/SlipcastingController.php
@@ -66,27 +66,34 @@ class SlipcastingController extends Controller
 					}
 						
 					
+				//$input['campaign_id'] = 7;
 				$returnObj['draw'] = intval($input['draw']);
 					
 				$queryCount = DB::table('manu_slipcasting')
-				->select(['manu_slipcasting.created_datetime']);
+				->select(['manu_slipcasting.created_datetime','manu_inventory.campaign_id as campaign_id'])
+				->join('manu_slipcasting_profile', 'manu_slipcasting.manu_slipcasting_profile_id', '=', 'manu_slipcasting_profile.manu_slipcasting_profile_id')
+				->join('manu_slipcasting_steel', 'manu_slipcasting_steel.manu_slipcasting_id', '=', 'manu_slipcasting.manu_slipcasting_id')
+				->Leftjoin('manu_slip_recipe', 'manu_slipcasting_steel.slip_id', '=', 'manu_slip_recipe.recipe_id')
+				->join('manu_inventory', 'manu_slipcasting_steel.inventory_id', '=', 'manu_inventory.manu_inventory_id')
+				->join('manu_campaign', 'manu_inventory.campaign_id', '=', 'manu_campaign.campaign_id');
 
 				
 				
 				// What can we search or filter by?
 				$SearchableConditionals = array('manu_slip_id', 'qti_id');
-				$FilterableConditionals = array();
-				
+				$FilterableConditionals = array('campaign_id' => 'manu_campaign.campaign_id');
+			
 						
 				$query  = DB::table('manu_slipcasting')
-				->select(['manu_slipcasting.manu_slipcasting_id', 'inventory_id', 'heat_id', 'manu_slipcasting.created_datetime', 'campaign_name','manu_slipcasting.manu_slipcasting_profile_id','profile_name'])
+				->select(['manu_slipcasting.manu_slipcasting_id', 'inventory_id', 'heat_id', 'manu_slipcasting.datetime', 'campaign_name','manu_slipcasting.manu_slipcasting_profile_id','profile_name', 'manu_slip_recipe.recipe_id', 'manu_slip_recipe.recipe_name', 'manu_campaign.campaign_id'])
 				->join('manu_slipcasting_profile', 'manu_slipcasting.manu_slipcasting_profile_id', '=', 'manu_slipcasting_profile.manu_slipcasting_profile_id')
 				->join('manu_slipcasting_steel', 'manu_slipcasting_steel.manu_slipcasting_id', '=', 'manu_slipcasting.manu_slipcasting_id')
 				->Leftjoin('manu_slip_recipe', 'manu_slipcasting_steel.slip_id', '=', 'manu_slip_recipe.recipe_id')
 				->join('manu_inventory', 'manu_slipcasting_steel.inventory_id', '=', 'manu_inventory.manu_inventory_id')
 				->join('manu_campaign', 'manu_inventory.campaign_id', '=', 'manu_campaign.campaign_id')
 				->skip($input['start'])
-				->take($input['length']);
+				->take($input['length']*2)
+				->orderBy('datetime','desc');
 					
 				
 					//Search value functionality
@@ -95,6 +102,7 @@ class SlipcastingController extends Controller
 								foreach($SearchableConditionals as $key)
 								{
 									
+								
 									$query->orWhere($key,'Like','%'.$input['search']['value'].'%');
 									$queryCount->orWhere($key,'Like','%'.$input['search']['value'].'%');
 									
@@ -104,13 +112,13 @@ class SlipcastingController extends Controller
 							}
 
 					//custom field functionality 	
-					foreach($FilterableConditionals as $key)
+					foreach($FilterableConditionals as $key =>$value)
 							{
 								if(isset($input[$key]) && strlen($input[$key]) > 0)
 								{
 									
-										$query->Where($key,'=',''.$input[$key].'');
-										$queryCount->Where($key,'=',''.$input[$key].'');
+										$query->Where($FilterableConditionals[$key],'=',''.$input[$key].'');
+										$queryCount->Where($FilterableConditionals[$key],'=',''.$input[$key].'');
 									
 								}
 								
@@ -124,39 +132,51 @@ class SlipcastingController extends Controller
 				$returnObj['recordsTotal'] = $queryCount;
 				$returnObj['recordsFiltered'] = $queryCount;
 					
-					
+					$last_slipcast_id = 0;
 				//Special Filtering for slipcast needs
+				
+				$omitt = array('inventory_id', 'heat_id','recipe_id','recipe_name');
+				
 				foreach($query as $slipcast)
 				{
 				
-					foreach($slipcast as $key => $value)
+					if($slipcast->manu_slipcasting_id != $last_slipcast_id)
 					{
-						if($key == 'inventory_id')
+						$tempArray = array();
+						
+						
+						foreach($slipcast as $key => $value)
 						{
-						$returnObj['aoData'][$slipcast->manu_slipcasting_id]['steel'][] = array( 'id' => $value, 'name' => $slipcast->heat_id);
+						
+							if(!in_array($key,$omitt))
+							{
+								$tempArray[$key] = $value;
+							}
+							
 						
 						
 						}
-						else if ($key == 'heat_id')
-						{
 						
-						
-						}
-						else
-						{
-							$returnObj['aoData'][$slipcast->manu_slipcasting_id][$key] = $value;
-						}
-						
-					
+						$tempArray['steel'][] = array('id'=>$slipcast->inventory_id,'heat_id'=>$slipcast->heat_id, 'recipe_id' => $slipcast->recipe_id, 'recipe_name' => $slipcast->recipe_name);
 					
 					}
+					else
+					{
+					
+						
+						array_pop($tempObj);
+							$tempArray['steel'][] = array('id'=>$slipcast->inventory_id,'heat_id'=>$slipcast->heat_id, 'recipe_id' => $slipcast->recipe_id, 'recipe_name' => $slipcast->recipe_name);
+						
+						
+					
+					}
+					$tempObj[] = $tempArray;
 				
-				
-				
+					$last_slipcast_id = $slipcast->manu_slipcasting_id;
 				}
 					
 					
-				$returnObj['aoData'] = $query; 
+				$returnObj['aoData'] = $tempObj; 
 				return response()->json($returnObj, 200);
 	
 	
