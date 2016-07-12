decaadee0941c8dcac8806bb9a5045b2c757aa15
chrispetrone <ctpetrone@gmail.com>
2016-07-12 13:58:26 -0600

    yay

--- app/Models/Slipcasting.php
+++ app/Models/Slipcasting.php
@@ -95,7 +95,7 @@ class Slipcasting extends Model
                 for ($k = 0; $k < $seriesCnt; $k++) {
                     $tempObj = array();
 
-                    $tempObj[0] = $arrays[$i][1] . " " . $arrays[$i][2];
+                    $tempObj[0] = strtotime($arrays[$i][1] . " " . $arrays[$i][2])*1000;
                     $tempObj[1] = (float) $arrays[$i][3 + $k];
 
                     $response->dataset[$k]->data[] = $tempObj;
@@ -149,8 +149,8 @@ class Slipcasting extends Model
                     $tempRow = preg_split('/[\s]+/', $rows[$i]);
                     $tempObj = array();
 
-                    $tempObj[0] = $tempRow[0].' '.$tempRow[1]. ' '.$tempRow[2];
-                    $tempObj[1] = $tempRow[$k+3];
+                    $tempObj[0] = strtotime($tempRow[0].' '.$tempRow[1]. ' '.$tempRow[2])*1000;
+                    $tempObj[1] = (float) $tempRow[$k+3];
 
                     $response  -> dataset[$k] -> data[] = $tempObj;
 
