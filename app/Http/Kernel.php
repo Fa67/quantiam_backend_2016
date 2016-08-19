39e09fa3b06e021bf598bcfd9d4b6bbd33f9fd20
chrispetrone <ctpetrone@gmail.com>
2016-08-19 09:37:47 -0600

    Logger fixxed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\RequestResponseLogger::class,
         ],
 
         'api' => [
@@ -50,6 +51,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'authuser' =>\App\Http\Middleware\JWTAuth::class,
+        \App\Http\Middleware\RequestResponseLogger::class,
 		
     ];
 }
--- app/Http/Middleware/Logger.php
+++ app/Http/Middleware/Logger.php
@@ -6,9 +6,10 @@ use Illuminate\Support\Facades\Log;
 
 use DB;
 use Closure;
+use Route;
 
 // Logs after request is fulfilled.
-class RequestResponseLogger 
+class RequestResponseLogger
 {
     /**
      * Handle an incoming request.
@@ -19,7 +20,26 @@ class RequestResponseLogger
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
+
+        if($request -> method() != 'GET' && isset($request -> user -> employeeid) && $request->path() != 'auth')
+        {
+            $routeParams = Route::getCurrentRoute()->parameters();
+            //dd($routeParams);
+            $params = array(
+                "method" => $request -> method(),
+                "path" => $request ->path(),
+                "route_parameters" => json_encode($routeParams),
+                "payload" => json_encode($request -> all()),
+                "userID" => $request -> user -> employeeid
+            );
+
+
+            DB::table('api_activity_log')->insert($params);
+        }
+
+
+        return $response;
     }
 
 }
